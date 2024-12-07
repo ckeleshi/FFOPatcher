d8d1fd9062bac1b628b37369ef0b01ef345344b6
ckeleshi <clanschen@gmail.com>
2024-12-07 10:47:42 +0800

    更新依赖库版本和注释

--- common/patch.cpp
+++ common/patch.cpp
@@ -4,11 +4,13 @@
 
 #pragma comment(lib, "winmm.lib")
 
+//提高Sleep函数的精度
 class time_period_guard
 {
 public:
     time_period_guard()
     {
+        std::memset(&_caps, 0, sizeof(_caps));
         timeGetDevCaps(&_caps, sizeof(_caps));
         timeBeginPeriod(_caps.wPeriodMin);
     }
@@ -22,6 +24,7 @@ private:
     TIMECAPS _caps;
 } g_period_guard;
 
+//提高时间戳的精度
 DWORD WINAPI accurate_timeGetTime()
 {
     return static_cast<DWORD>(std::chrono::duration_cast<std::chrono::milliseconds>(
@@ -37,15 +40,7 @@ UINT ReadInterval(HMODULE module)
     cppPath = cPath;
     cppPath = cppPath.parent_path() / L"ffopatcher.ini";
 
-    auto interval = GetPrivateProfileIntW(L"FFOPatcher", L"FrameInterval", 29, cppPath.wstring().c_str());
-
-    if (interval < 1)
-        interval = 1;
-
-    if (interval > 29)
-        interval = 29;
-
-    return interval;
+    return std::clamp(GetPrivateProfileIntW(L"FFOPatcher", L"FrameInterval", 15u, cppPath.wstring().c_str()), 1u, 29u);
 }
 
 void Patch(HMODULE module)
@@ -54,6 +49,7 @@ void Patch(HMODULE module)
 
     byte_pattern patterner;
 
+    //修改帧间隔，0x1D为游戏默认值29
     patterner.find_pattern("83 F8 1D 73 0D 6A 1D");
     if (patterner.has_size(1))
     {
@@ -61,6 +57,7 @@ void Patch(HMODULE module)
         injector::WriteMemory<unsigned char>(patterner.get_first().i(6), frame_interval, true);
     }
 
+    //完全替换游戏使用的所有timeGetTime函数为较高精度版本
     patterner.find_pattern("8B 35 ? ? ? ? 59 FF D6");
     if (patterner.has_size(1))
     {
